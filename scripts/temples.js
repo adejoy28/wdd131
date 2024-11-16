c0dc35b4f937c25c338f79d73e368192b3b83a9a
adejoy28 <adejoy28@gmail.com>
2024-11-16 16:52:56 +0100

    more updates

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,20 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+
+// Display for footers
+const today = new Date();
+const currentYear = document.getElementById("Year");
+
+// Display the current year in the footer section
+currentYear.innerHTML = today.getFullYear();
+
+// Display last modified date
+const lastModifiedDate = document.getElementById("lastModified");
+
+// Display the current year in the footer section
+lastModifiedDate.innerHTML = document.lastModified;
+
+
+
