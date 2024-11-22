53d134510e5b580ef215436f0a70eeaeaa38f0c5
adejoy28 <adejoy28@gmail.com>
2024-11-23 00:37:16 +0100

    more update

--- scripts/place.js
+++ scripts/place.js
@@ -1,6 +1,22 @@
+// Display windchill factor in the 
 
+let T, v, windChill;
 
+// Function to calculate windchill
+function calculateWincdChill(T, v) {
+    return 12.12 + 0.6215 * T - 11.37 * Math.pow(v, 0.16) + 0.3965 * T * Math.pow(v, 0.16);
+}
+if (T < 10 && v > 4.8) {
+    document.querySelector("#wind").innerHTML = calculateWincdChill(10,5);
+} else {
+    document.querySelector("#wind").innerHTML = "N/A";
+}
 
+// 13.12 + 0.6215×T−11.37×V
+// 0.16
+//     + 0.3965×T×V
+// 0.16
+// 35.74 + 0.6215T - 35.75v ^ 0.16 + 0.4275Tv ^ 0.16
 
 // Footer Script to display last modification timestamp
 const today = new Date();
